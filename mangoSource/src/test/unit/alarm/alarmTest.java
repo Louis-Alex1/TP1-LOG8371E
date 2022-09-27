428c200f38682782c0f01e35ea2f19c9244d2bfa
jecled <jerome.cleris@polymtl.ca>
2022-09-27 16:07:10 -0400

    basic alarm test

---
+++ mangoSource/src/test/unit/alarm/alarmTest.java
@@ -0,0 +1,20 @@
+package test.unit.alarm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.serotonin.mango.rt.event.AlarmLevels;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class alarmTest {
+
+    @Test
+    @DisplayName("Alarm Levels Description test")
+    void alarmLevelsDescriptionTest() {
+        assertEquals(AlarmLevels.getAlarmLevelDescription(0), AlarmLevels.NONE_DESCRIPTION);
+        assertEquals(AlarmLevels.getAlarmLevelDescription(1), AlarmLevels.INFORMATION_DESCRIPTION);
+        assertEquals(AlarmLevels.getAlarmLevelDescription(2), AlarmLevels.URGENT_DESCRIPTION);
+        assertEquals(AlarmLevels.getAlarmLevelDescription(3), AlarmLevels.CRITICAL_DESCRIPTION);
+        assertEquals(AlarmLevels.getAlarmLevelDescription(4), AlarmLevels.LIFE_SAFETY_DESCRIPTION);
+    }
+}
